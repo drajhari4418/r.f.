7b1f8e68eb82a573845d0d969839e173c618e553
drajhari4418 <drajhari4418@gmail.com>
2022-04-08 20:35:08 +0530

    first commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC7UBiMCQFzm80DkTtdw0WW1eLXX0paS5w",
+  authDomain: "intergating-firebase.firebaseapp.com",
+  projectId: "intergating-firebase",
+  storageBucket: "intergating-firebase.appspot.com",
+  messagingSenderId: "435456931987",
+  appId: "1:435456931987:web:5d992e7c0a35e2cd06f566"
+};
+
+export default firebaseConfig;
